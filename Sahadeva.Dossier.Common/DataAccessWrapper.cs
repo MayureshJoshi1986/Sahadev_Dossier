01dd79fb2b70cf50c3d8f3f0288b15d62cbd4b19
Arnold Pinto <Arnold.Pinto@necsws.com>
2024-08-31 01:09:45 +0530

    migrate DAL, Common and Entities to netstandard2.0

--- Sahadeva.Dossier.Common/DataAccessWrapper.cs
+++ Sahadeva.Dossier.Common/DataAccessWrapper.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
-using Microsoft.Practices.EnterpriseLibrary.Data;
-using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
-using Microsoft.Practices.EnterpriseLibrary.Common;
 
 namespace Sahadeva.Dossier.Common
 {
